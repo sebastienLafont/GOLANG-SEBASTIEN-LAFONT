0b61cbe851560686fbc9e9d434757f01e37815d8
sebastienLafont <103418568+sebastienLafont@users.noreply.github.com>
2022-04-14 16:06:02 +0200

    Add files via upload
    examen golang sébastien lafont

---
+++ exam.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+	"runtime"
+)
+
+type task struct {
+Descrition string
+Done bool
+
+
+}
+const (
+	MethodPost ="POST"
+)
+
+type Request struct {
+
+	Method string
+}
+
+
+
+
+ 
+func main(){
+	vartask := []string{("faire ses course" id:false) "payer les factures" id:false) Done == false} 
+
+}
+
+rw.WriteHeader(http.StatusOK)
+Write([]byte) (int, error)
+chaine="write chaine"
+enBytes:= []byte(chaine)
+
+}
+for vartask = range vartask 
+if f == id{
+	id = false 
+	id = f
+	break
+}
+return id
+
+hnl1 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "bonjour, voici un func #1!\n")
+		rw.WriteHeader(http.StatusBadRequest)
+
+	}
+
+	hnl2 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "bonjour, voici un func #2!\n")
+		rw.WriteHeader(http.StatusBadRequest)
+
+	}
+	hnl3 := func(w http.ResponseWriter, _ *http.Request) {
+		rw.WriteHeader(http.StatusBadRequest)
+
+	
+		io.WriteString(w, "bonjour, voici un func #3!\n")
+	}
+
+	http.HandleFunc("/", list, hnl1)
+	http.HandleFunc("/done", done, hnl2)
+	http.HandleFunc("/add", add, hnl3)
+}
+
+helloHandler := func(w http.ResponseWriter, req *http.Request)
+
+}
+	io.WriteString(w, "ceci est un listen and serve\n")
+
+}
+}
+func list(rw http.ResponseWriter, _ *http.Request)
+
+http.HandleFunc("/", /)
+log.Fatal(http.ListenAndServe(":4000", nil))
+}
+http.HandleFunc("/done", done)
+log.Fatal(http.ListenAndServe(":4000", nil))
+}
+http.HandleFunc("/add", add)
+log.Fatal(http.ListenAndServe(":4000", nil))
+
+body, err :=
+ioutil.ReadAll(r.Body) if err !=
+nil {
+fmt.Printf("Erreur lecture du body : %v", err)
+http.Error(
+rw,
+"impossible de lire body", http.StatusBadRequest,
+)
+return
+body, err:= strconvItoa()
+var task body(description)
+
+if r.Method == http.MethodGet {
+	io.WriteString(w, "Ceci est une requête ")
+} else if r.Method == http.MethodPost {
+	io.WriteString(w, "Ceci est une requête")
+} else {
+	io.WriteString(w, "Ceci est une " + r.Method + " requête")
+}
+
+
+var b [OK]int
+b = append 
+break
+
+
+switch = r.method{
+case "r.method"
+fmt.println("r.method")
+default 
+//BadRequest
+
+}
+func list(r.method, done = false)
+
+body :=strconv.Itoa()
+
+	
+
+
+
+
+
+
+}
+
+
+
+
